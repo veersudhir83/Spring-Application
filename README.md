3c7bef7a387648bc7ffcadf4be81cbca77ed7def
veersudhir83 <veersudhir83@gmail.com>
2017-10-10 22:49:45 +0530

    Converted to spring

---
+++ README.md
@@ -0,0 +1,2 @@
+# spring_test
+Spring Examples
---
+++ src/main/java/io/sudheer/pluralsight/service/CustomerServiceImpl.java
@@ -0,0 +1,29 @@
+package io.sudheer.pluralsight.service;
+
+import java.util.List;
+
+import io.sudheer.pluralsight.model.Customer;
+import io.sudheer.pluralsight.repository.CustomerRepository;
+
+public class CustomerServiceImpl implements CustomerService {
+	
+	private CustomerRepository customerRepository;
+	
+	public CustomerServiceImpl() {
+
+	}
+	
+	public CustomerServiceImpl(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
+	
+	@Override
+	public List<Customer> findAll() {
+		return customerRepository.findAll();
+	}
+
+	public void setCustomerRepository(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
+
+}
