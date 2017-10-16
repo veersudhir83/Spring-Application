08f865259d124632e0cf0c8964c9a7a2fbf52438
veersudhir83 <veersudhir83@gmail.com>
2017-10-16 14:26:48 +0530

    complete Spring Fundamentals - using properties files and Scopes

--- src/main/java/io/sudheer/pluralsight/repository/HibernateCustomerRepositoryImpl.java
+++ src/main/java/io/sudheer/pluralsight/repository/HibernateCustomerRepositoryImpl.java
@@ -3,15 +3,22 @@ package io.sudheer.pluralsight.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import io.sudheer.pluralsight.model.Customer;
 
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
+	
+	@Value("${dbUserName}")
+	private String dbUserName;
 
 	/* (non-Javadoc)
 	 * @see io.sudheer.pluralsight.repository.CustomerRepository#findAll()
 	 */
 	@Override
 	public List<Customer> findAll() {
+		
+		System.out.println("DB User Name: " + dbUserName);
 		List<Customer> customers = new ArrayList<>();
 		
 		Customer customer = new Customer();
@@ -20,5 +27,9 @@ public class HibernateCustomerRepositoryImpl implements CustomerRepository {
 		customers.add(customer);
 		
 		return customers;
+	}
+
+	public void setDbUserName(String dbUserName) {
+		this.dbUserName = dbUserName;
 	}
 }
--- src/main/java/io/sudheer/pluralsight/service/CustomerServiceImpl.java
+++ src/main/java/io/sudheer/pluralsight/service/CustomerServiceImpl.java
@@ -2,6 +2,8 @@ package io.sudheer.pluralsight.service;
 
 import java.util.List;
 
+import org.springframework.context.annotation.Scope;
+
 import io.sudheer.pluralsight.model.Customer;
 import io.sudheer.pluralsight.repository.CustomerRepository;
 
